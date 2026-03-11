93f4a5154c36e052db0d2ff7fff0a6415b65f351
Joshua Santana <joshuasantana73@gmail.com>
2026-03-11 12:03:20 -0400

    added users page

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const userRouter = require('./routes/users')
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -7,17 +8,31 @@ app.use(express.static('public'));
 app.listen(3030);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/users',userRouter);
+
 // GET /submit - logs query parameters
 app.get('/submit', (req, res) => {
 console.log('--- GET Request Received ---');
 console.log('Query Parameters:', req.query);
+
 Object.entries(req.query).forEach(([key, value]) => {
 console.log(` ${key}: ${value}`);
 });
+
+
+app.get('/solution', (req, res)=> {
+let num1 = parseInt(req.query['num1']);
+let num2 = parseInt(req.query['num2']);
+let total = num1 +num2;
+res.send(`<h2>${num1} + ${num2} = ${total} </h2>`);
+});
+
 console.log('----------------------------');
 res.send('<h2>GET request received!</h2><pre>' + JSON.stringify(req.query, null,
 2) + '</pre>');
 });
+
+
 // POST /submit - logs body contents
 app.post('/submit', (req, res) => {
 console.log('--- POST Request Received ---');
@@ -25,10 +40,14 @@ console.log('Body:', req.body);
 Object.entries(req.body).forEach(([key, value]) => {
 console.log(` ${key}: ${value}`);
 });
+
+
 console.log('-----------------------------');
 res.send('<h2>POST request received!</h2><pre>' + JSON.stringify(req.body, null,
 3) + '</pre>');
 });
+
+
 app.listen(3030, () => {
 console.log('Server running on http://localhost:3030');
 });
