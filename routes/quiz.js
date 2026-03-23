fc6632482f2bd4bed06a1d009c01db45ad6a00dc
Joshua Santana <joshuasantana73@gmail.com>
2026-03-23 11:51:50 -0400

    Make functions for random parts

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const {readFile} = require('fs').promises;
+
+router.get("/", (req, res) =>{
 
 
+});
 
+let getWords = async ()=>{
+    //get random part of speeh
+    
+    //based on that, pick 4 words that match
+}
+let getRandom = ()=>{
+    let parts = ['noun','verb','adj']
+    let randomIndex = Math.floor(Math.random()*parts.length);
+    let randomPart = parts[randomIndex];
+    return randomPart;
+}
 
+let shuffle = (array)=>{
+    //fisher yates algorithm
+    for (let i = 0;i < array;i--)
+    {
+        let randomNumber = Math.floor(Math.random()*(i+1))
+        [array[i], array[randomNumber]] = [array[randomNumber], array[i]];
+    }
+}
 
 
 
