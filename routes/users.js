faf04ed1bd68c2d20f4d555be7c994e65e82995d
Joshua Santana <joshuasantana73@gmail.com>
2026-05-08 05:47:04 -0400

    completed all requirements for  user upload homework

--- routes/users.js
+++ routes/users.js
@@ -8,10 +8,13 @@ router.route('/').get ((req, res) =>{
     res.send('User List');
 }).post((req, res)=>{
     const firstName  = req.body.firstName;
-    const isValid = firstName !=="";
+    const lastName = req.body.lastName;
+    const age = req.body.age;
+    const gender = req.body.gender;
+    const isValid = firstName && lastName && age !=="";
     if(isValid){
-        console.log(`Adding user: ${firstName}`)
-        users.push({firstName});
+        console.log(`Adding user: ${firstName},${lastName},${age},${gender}`)
+        users.push({firstName,lastName,age,gender});
         res.render('users/list', {users});
     }
     else{
@@ -23,7 +26,7 @@ router.get('/list', (req,res) =>{
     res.render('users/list',{users})
 });
 router.get('/new', (req, res)=>{
-    res.render('users/new', {firstName:"Test"})
+    res.render('users/new', {firstName:"first"})
 
 });
 //router.get('/:id', (req, res)=>{
@@ -32,14 +35,14 @@ router.get('/new', (req, res)=>{
 router.route('/:id').get((req, res)=>{
     console.log(req.user);
     console.log('Getting user data!')
-    res.send(`Getting User data for id: ${req.user['name']}`);
+    res.render('users/Info', {user: req.user});
 }).delete((req, res)=>{
     res.send(`Deleting User data for id: ${req.params.id}`); 
 }).put((req, res)=>{
     res.send(`Updating User data for id: ${req.params.id}`);
 });
 
-const users = [{firstName:"Joshua"}, {firstName:"Katie"}]
+const users = [{firstName:"Joshua", lastName:"Santana", age: 27, gender: "male"}, {firstName:"Katie", lastName:"Owens", age:24, gender:"female"}]
 router.param("id", (req, res, next, id) =>{
     req.user = users[id];
     next();
