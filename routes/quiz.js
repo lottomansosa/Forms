efa7e76a1770f86cb9cf32537bc0be23e549c424
Joshua Santana <joshuasantana73@gmail.com>
2026-05-10 21:33:46 -0400

    Finished word quiz assignment

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,72 +1,68 @@
 const express = require('express');
 const router = express.Router();
 const {readFile} = require('fs').promises;
-//Work goes here
-
-router.post("/", (req, res)=>{
-    console.log(req.body);
+ 
+router.post("/", async (req, res)=>{
     let {userChoice, correctDef, totalQuestions, totalCorrect} = req.body;
-    if(userChoice === correctDef)
-    {
-        console.log("User guessed Correctly!");
-        let score = totalCorrect+1;
+ 
+    // Convert to numbers since form data comes in as strings
+    totalQuestions = parseInt(totalQuestions) + 1;
+    totalCorrect = parseInt(totalCorrect);
+ 
+    // Check if the user got it right
+    let isCorrect = userChoice === correctDef;
+    if(isCorrect){
+        totalCorrect++;
     }
-    let total = totalQuestions+1;
-    //get another new set of words...how?
-    //send that set of words back with score
-    //send other data back
-});
-
-router.get("/", async (req, res) =>{
-    //Get 5 words, with their pos and def and send back to the other page
+ 
+    // Get a new set of words for the next question
     let chosenWords = await getWords();
-    //send those back and render quiz.ejs
-    console.log("Chosen Words: ", chosenWords);
-    res.render('quiz', {chosenWords});
+ 
+    // Send everything back to the page
+    res.render('quiz', {chosenWords, totalQuestions, totalCorrect, isCorrect, correctDef});
 });
-
+ 
+router.get("/", async (req, res) =>{
+    let chosenWords = await getWords();
+    console.log("Chosen Words: ", chosenWords);
+    // No result to show on first load
+    res.render('quiz', {chosenWords, totalQuestions: 0, totalCorrect: 0, isCorrect: null, correctDef: null});
+});
+ 
 let getWords = async ()=>{
-    //get a random part of speech
     console.log("Getting random Part!")
-    let randomPart = getRandomPart(); // i should have noun, verb, or adjective
-    //based on that, pick 4 words that match
+    let randomPart = getRandomPart();
     console.log("Random part: ", randomPart);
-    let allWords = await readFile('resources/allwords.txt', 'utf8'); //Reads allwords as 1 giant string
-    // console.log(allWords);
-    let wordArray = allWords.split('\n'); //splits the single string into an array where each line is an index
-    // console.log(wordArray);
-    shuffle(wordArray);//shuffle that array
-
+    let allWords = await readFile('resources/allwords.txt', 'utf8');
+    let wordArray = allWords.split('\n');
+    shuffle(wordArray);
+ 
     let choices = [];
-    while(choices.length < 5) { //keep looping until we get 5 choice
-        let line = wordArray.pop(); //one line as a string
-        // let [word, part, def] = line.split('\t'); This is the same as the code below
+    while(choices.length < 5){
+        let line = wordArray.pop();
         let tokens = line.split('\t');
         let word = tokens[0];
         let part = tokens[1];
         let def = tokens[2];
-        if(part === randomPart){ //If the part of my word matches the random part we picked, we keep it
+        if(part === randomPart){
             choices.push(line);
         }
     }
     return choices;
-
 }
-
+ 
 let getRandomPart = ()=>{
     let parts = ['noun', 'verb', 'adjective'];
     let randomIndex = Math.floor(Math.random()*parts.length);
-    let randomPart = parts[randomIndex];
-    return randomPart;
+    return parts[randomIndex];
 }
+ 
 let shuffle = (array)=>{
-    //fisher Yates algorithm
-    for(let i = array.length-1;i >0;i--)
-    {
+    for(let i = array.length-1; i > 0; i--){
         let randomNumber = Math.floor(Math.random()*(i+1));
-        [array[i], array[randomNumber]]  = [array[randomNumber], array[i]];
+        [array[i], array[randomNumber]] = [array[randomNumber], array[i]];
     }
     console.log("Array shuffled!");
 }
-
+ 
 module.exports = router;
